1b74be47ee8103de73d3948c3c80e876267fa69e
Cenk Ebret <cenk.ebret@frontmen.nl>
2019-10-21 09:36:39 +0200

    fix(master): added documentation to request & response, and added missing definitions to request

--- src/request.d.ts
+++ src/request.d.ts
@@ -19,4 +19,105 @@ export class Request extends Readable {
     xhr: boolean;
     event: APIGatewayProxyEvent;
     accept: Accepts;
+    /**
+     * Check if the given `type(s)` is acceptable, returning
+     * the best match when true, otherwise `undefined`, in which
+     * case you should respond with 406 "Not Acceptable".
+     *
+     * The `type` value may be a single MIME type string
+     * such as "application/json", an extension name
+     * such as "json", a comma-delimited list such as "json, html, text/plain",
+     * an argument list such as `"json", "html", "text/plain"`,
+     * or an array `["json", "html", "text/plain"]`. When a list
+     * or array is given, the _best_ match, if any is returned.
+     *
+     * Examples:
+     *
+     *     // Accept: text/html
+     *     req.accepts('html');
+     *     // => "html"
+     *
+     *     // Accept: text/*, application/json
+     *     req.accepts('html');
+     *     // => "html"
+     *     req.accepts('text/html');
+     *     // => "text/html"
+     *     req.accepts('json, text');
+     *     // => "json"
+     *     req.accepts('application/json');
+     *     // => "application/json"
+     *
+     *     // Accept: text/*, application/json
+     *     req.accepts('image/png');
+     *     req.accepts('png');
+     *     // => undefined
+     *
+     *     // Accept: text/*;q=.5, application/json
+     *     req.accepts(['html', 'json']);
+     *     req.accepts('html', 'json');
+     *     req.accepts('html, json');
+     *     // => "json"
+     *
+     * @param {String|Array} type(s)
+     * @return {String|Array|Boolean}
+     * @public
+    */
+    accepts(args: string | string[]): string | boolean | string[];
+    /**
+     * Check if the given `encoding`s are accepted.
+     * @param {String|Array} ...encoding
+     * @return {String|Array}
+    */
+    acceptsLanguages(args: string | string[]): string | string[];
+    /**
+     * Return request header.
+     *
+     * The `Referrer` header field is special-cased,
+     * both `Referrer` and `Referer` are interchangeable.
+     *
+     * Examples:
+     *
+     *     req.get('Content-Type');
+     *     // => "text/plain"
+     *
+     *     req.get('content-type');
+     *     // => "text/plain"
+     *
+     *     req.get('Something');
+     *     // => undefined
+     *
+     * Aliased as `req.header()`.
+     *
+     * @param {String} key
+     * @return {String}
+    */
+    get(key: string): string | undefined;
+
+    header(name: string): string | undefined;
+
+    /**
+     * Check if the incoming request contains the "Content-Type"
+     * header field, and it contains the give mime `type`.
+     *
+     * Examples:
+     *
+     *      // With Content-Type: text/html; charset=utf-8
+     *      req.is('html');
+     *      req.is('text/html');
+     *      req.is('text/*');
+     *      // => true
+     *
+     *      // When Content-Type is application/json
+     *      req.is('json');
+     *      req.is('application/json');
+     *      req.is('application/*');
+     *      // => true
+     *
+     *      req.is('html');
+     *      // => false
+     *
+     * @param {String|Array} types...
+     * @return {String|false|null}
+    */
+   is(types: string | string[]): string | boolean | null;
 }
