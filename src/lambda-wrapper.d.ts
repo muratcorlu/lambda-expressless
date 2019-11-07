1742476db6b29c60c09888fa9ea0ed6de21810a1
sanderphilipse <sander.philipse@essent.nl>
2019-11-07 11:34:32 +0100

    fixed typing for middlewares to be the express typing for middlewares

--- src/lambda-wrapper.d.ts
+++ src/lambda-wrapper.d.ts
@@ -1,12 +1,9 @@
 import { Request } from './request';
 import { Response } from './response';
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { RequestHandler } from 'express';
 
-export interface Middleware {
-    (request: Request, response: Response, next: Function): void
-}
-
-export function use(...handlers: Middleware[]): ApiGatewayProxyHandler;
+export function use(...handlers: RequestHandler[]): APIGatewayProxyHandler;
 
 export { Request } from './request';
 export { Response } from './response';
