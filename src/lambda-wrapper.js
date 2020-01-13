6755fcd22da9f44ba3ec4885e1559e5d70f8d017
Tomas Polach <tomas@polach.ch>
2020-01-13 14:56:26 +0100

    Use Lambda Promise API

--- src/lambda-wrapper.js
+++ src/lambda-wrapper.js
@@ -1,18 +1,19 @@
 const { Response } = require('./response');
 const { Request } = require('./request');
 
-exports.use = (router) => {
-  return (event, context, callback) => {
-    context.callbackWaitsForEmptyEventLoop = false;
+exports.apiGatewayHandler = (router) => {
+  return function handleApiGatewayEvent(event, context) => {
+    return new Promise((resolve) => {
 
-    const req = new Request(event);
-    const res = new Response(request);
-    req.res = res;
-
-    router.handle(req, res, () => {
-      // Set response to Lambda.
-      // This will cause Lambda to freeze and should be executed last
-      callback(res.responseObj)
+      const req = new Request(event);
+      const res = new Response(request);
+      req.res = res;
+  
+      router.handle(req, res, () => {
+        // Set response to Lambda.
+        // This will cause Lambda to freeze and should be executed last
+        resolve(res.responseObj)
+      })
     })
   }
 }
