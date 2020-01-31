647f01024a1f0c6ffb9e4626ac33e7b34639334c
Murat Corlu <muratcorlu@me.com>
2020-01-31 15:28:08 +0100

    fix: Proper type definition for returned callback

--- src/lambda-wrapper.d.ts
+++ src/lambda-wrapper.d.ts
@@ -1,8 +1,11 @@
 import { Request } from './request';
 import { Response } from './response';
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
 import { NextFunction, NextHandleFunction } from "connect";
 
+export interface APIGatewayProxyHandler {
+  (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult>
+}
 
 export interface Middleware extends NextHandleFunction {
     (request: Request, response: Response, next: NextFunction): void
